43f9a6bfde56ca17a4120068e82a0510c72a5350
Daekyung Kim <timkim0713@gmail.com>
2021-08-10 21:20:07 +0900

    add button

--- hw3/calc.js
+++ hw3/calc.js
@@ -1,3 +1,62 @@
 /*
  * Implement all your JavaScript in this file!
  */
+
+var userNumbers = [];
+
+$("#clearButton").click(function () {
+    $("#display").val("");
+});
+
+
+$("#button0").click(function () {
+    $("#display").val($("#display").val() + "0");
+});
+
+
+$("#button1").click(function () {
+    $("#display").val($("#display").val() + "1");
+});
+
+$("#button2").click(function () {
+    $("#display").val($("#display").val() + "2");
+});
+
+$("#button3").click(function () {
+    $("#display").val($("#display").val() + "3");
+});
+
+$("#button4").click(function () {
+    $("#display").val($("#display").val() + "4");
+});
+
+$("#button5").click(function () {
+    $("#display").val($("#display").val() + "5");
+});
+
+$("#button6").click(function () {
+    $("#display").val($("#display").val() + "6");
+});
+
+$("#button7").click(function () {
+    $("#display").val($("#display").val() + "7");
+});
+
+$("#button8").click(function () {
+    $("#display").val($("#display").val() + "8");
+});
+
+$("#button9").click(function () {
+    $("#display").val($("#display").val() + "9");
+});
+
+
+$("#addButton").click(function () {
+    if (userNumbers.length == 0) {
+        userNumbers.push($('#display').val())
+        $("#display").val("");
+    } else {
+        $("#display").val(parseInt($("#display").val()) + parseInt(userNumbers[0]));
+        userNumbers.pop()
+    }
+});
