8e3652d10a856cfd1ee2516f330f1f20339f5f07
Daekyung Kim <timkim0713@gmail.com>
2021-08-13 18:34:51 +0900

    a

--- hw4/FontChooser.js
+++ hw4/FontChooser.js
@@ -2,18 +2,59 @@ class FontChooser extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			boldHidden: true,
+			decreaseButtonHidden: true,
+			fontSizeSpanHidden: true,
+			increaseButtonHidden: true,
+			toggleForm: true,
+
+
+			toggleBold: false,
+
+
+		};
+	}
+
+	toggleForm = () => {
+		if (this.state.toggleForm == true) {
+			this.setState({ toggleForm: false })
+
+			this.setState({ boldHidden: true })
+			this.setState({ decreaseButtonHidden: true })
+			this.setState({ fontSizeSpanHidden: true })
+			this.setState({ increaseButtonHidden: true })
+
+		} else {
+			this.setState({ toggleForm: true })
+
+			this.setState({ boldHidden: false })
+			this.setState({ decreaseButtonHidden: false })
+			this.setState({ fontSizeSpanHidden: false })
+			this.setState({ increaseButtonHidden: false })
+		}
 	}
 
 
-	render() {
+	toggleBold = () => {
+		if (this.state.toggleBold == true) {
+			this.setState({ toggleBold: false })
+		} else {
+			this.setState({ toggleBold: true })
+		}
+	}
 
+	render() {
 		return (
 			<div>
-				<input type="checkbox" id="boldCheckbox" hidden='true' />
-				<button id="decreaseButton" hidden='true'>-</button>
-				<span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-				<button id="increaseButton" hidden='true'>+</button>
-				<span id="textSpan">{this.props.text}</span>
+				<input type="checkbox" id="boldCheckbox" onChange={this.toggleBold}
+					style={this.state.toggleBold ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}
+					hidden={this.state.boldHidden} />
+				<button id="decreaseButton" hidden={this.state.decreaseButtonHidden}>-</button>
+				<span id="fontSizeSpan" hidden={this.state.fontSizeSpanHidden}>{this.props.size}</span>
+				<button id="increaseButton" hidden={this.state.increaseButtonHidden}>+</button>
+				<span id="textSpan" onClick={this.toggleForm}>{this.props.text}</span>
 			</div>
 		);
 	}
